bb6cdfa1860b3364214434b4540822285be1dac9
abdabdabdabdabdabdabd321-hub <abdabdabdabdabdabdabd321@gmail.com>
2026-06-18 11:27:21 +0300

    Update index.js
    Update index.js with server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("StudentBag Backend");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end("StudentBag Backend Running");
+});
+
+server.listen(3000, () => {
+
+    console.log(
+        "Server running on port 3000"
+    );
+});
